85b66881352d2294898016f5bf3a510d34917473
Hector <your-email@example.com>
2025-10-19 22:34:32 -0700

    update

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 declare global {
   // eslint-disable-next-line no-var
@@ -16,4 +16,4 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export type { PrismaClient }
-export { Prisma } from '@prisma/client'
+export { Prisma }
